2da70d8410970323bfadd3f8f90d3f2b7a8d41fb
Chris <CopOnTheRun@gmail.com>
2013-08-09 21:08:19 -0400

    Updated parser.py to include the 'FbMsg' class, and some magic methods

--- parser.py
+++ parser.py
@@ -1,5 +1,17 @@
 from bs4 import BeautifulSoup as b
 
+class FbMsg(object):
+    """Contains a list of Threads"""
+
+    def __init__(self, fileName):
+        self.threads = []
+        soup = b(open(fileName))
+        for x in soup.find_all(class_='thread'):
+            self.threads.append(Thread(x))
+    
+    def __getitem__(self, key):
+        return self.threads[key]
+
 class Thread(object):
     """Contains a list of messages, as well as participants"""
 
@@ -8,9 +20,13 @@ class Thread(object):
         self.messages=[]
         for x in threadDiv.find_all(class_='message'):
             self.messages.append(Message(x,x.next_sibling))
+    
+    def __getitem__(self, key):
+        return self.messages[key]
 
     def __str__(self):
         return '\nA conversation between {}\n{}\n{}'.format(self.people,self.messages,'~'*79)
+
     def __repr__(self):
         return '\nA conversation between {}\n{}\n{}'.format(self.people,self.messages,'~'*79)
 
@@ -21,17 +37,9 @@ class Message(object):
         self.text = pDiv.string
         self.sender = messDiv.find(class_='user').string
         self.timeDate = messDiv.find(class_='meta').string
+        
 
     def __str__(self):
         return 'On {} {} said: {}'.format(self.timeDate,self.sender,self.text)
     def __repr__(self):
         return '\n{}\n{}\n{}'.format(self.timeDate,self.sender,self.text)
-
-def main():
-    threads = []
-    soup = b(open('messages.htm'))
-    for x in soup.find_all(class_='thread'):
-        threads.append(Thread(x))
-    print(threads)
-
-main()
